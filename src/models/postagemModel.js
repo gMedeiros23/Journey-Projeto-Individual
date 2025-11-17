3ea95d9435061dc21669334503870f4341938e5b
gMedeiros23 <gabriel.nascimento@sptech.school>
2025-11-17 15:08:02 -0300

    Forum - Rota GET - Realizando a solicitação e guardando informações no vetor post

--- src/models/postagemModel.js
+++ src/models/postagemModel.js
@@ -5,16 +5,15 @@ function listar() {
     u.nome,  
     p.conteudo AS postagem_conteudo, 
     p.data AS postagem_data, 
-    cp.conteudo AS comentario_conteudo, 
-    cp.data AS comentario_data, 
+    COUNT(DISTINCT cp.id_comentarios_postagem) AS quantidade_comentarios,  
     COUNT(c.id_curtidas) AS quantidade_curtidas
-FROM usuario u
-JOIN postagem p ON p.fkusuario = u.id_usuario
-LEFT JOIN comentarios_postagem cp ON cp.fkpostagem = p.id_postagem
-LEFT JOIN curtidas c ON c.fkpostagem = p.id_postagem
-GROUP BY p.id_postagem, cp.id_comentarios_postagem, cp.conteudo, cp.data, u.nome, p.conteudo, p.data
-ORDER BY p.data
-LIMIT 10;`;
+    FROM usuario u
+    JOIN postagem p ON p.fkusuario = u.id_usuario
+    JOIN comentarios_postagem cp ON cp.fkpostagem = p.id_postagem
+    JOIN curtidas c ON c.fkpostagem = p.id_postagem
+    GROUP BY p.id_postagem, u.nome, p.conteudo, p.data
+    ORDER BY p.data
+    LIMIT 5;`;
 
     return database.executar(instrucaoSql);
 }
